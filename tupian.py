cb241161bc64482e07e92c158dbcfbc15fcc7cca
lcc-666 <2502247952@qq.com>
2022-05-06 19:47:41 +0800

    封装进入一个类

--- tupian.py
+++ tupian.py
@@ -1,7 +1,7 @@
 import base64
 import os
 import time
-
+import tqdm
 import requests
 from lxml import etree
 from selenium import webdriver
@@ -14,35 +14,40 @@ class down:
     def __init__(self, url):
         self.bro = webdriver.Chrome()
         self.root_url = url
-        ls = self.get_list()
-        for item in ls:
-            print(item)
-            self.detail(item)
+        for i in range(1, 3):
+            ls = self.get_list(i)
+            for item in ls:
+                print("第{}页第{}个".format(i, ls.index(item) + 1), item)
+                self.detail(item)
 
-    def get_list(self):
+    def get_list(self, page):
         root_url = self.root_url
-        tagre_url = "{}/tupian/list-%E6%B8%85%E7%BA%AF%E5%94%AF%E7%BE%8E-1.html".format(root_url)
+        tagre_url = "{}/tupian/list-%E6%B8%85%E7%BA%AF%E5%94%AF%E7%BE%8E-{}.html".format(root_url, page)
         bro = requests.session()
         text = bro.get(tagre_url).text
         tree = etree.HTML(text)
         url_list = tree.xpath("//div[@id='tpl-img-content']/li/a/@href")
-        url_list=url_list
-        l2=list(set(url_list))
+        url_list = url_list
+        l2 = list(set(url_list))
         l2.sort(key=url_list.index)
 
         ls = []
         for i in range(20):
             ls.append(root_url + l2[i])
-
         return ls
 
-    def detail(self,url):
-        bro=self.bro
+    def detail(self, url):
+        bro = self.bro
         bro.get(url)
-        WebDriverWait(bro, 10, 0.5).until(EC.presence_of_element_located((By.CLASS_NAME, "videopic")))
+        try:
+            WebDriverWait(bro, 10, 0.5).until(EC.presence_of_element_located((By.CLASS_NAME, "videopic")))
+        except:
+            bro.get(url)
+            WebDriverWait(bro, 10, 0.5).until(EC.presence_of_element_located((By.CLASS_NAME, "videopic")))
+
         ls = bro.find_elements(by=By.CLASS_NAME, value="videopic")
 
-        for item in ls:
+        for item in tqdm.tqdm(ls):
             item.click()
             time.sleep(0.5)
             txt = item.get_property("src")
@@ -52,7 +57,7 @@ class down:
             file.close()
             self.getpic(name)
 
-    def getpic(self,name):
+    def getpic(self, name):
         path = r"D:\learn\picture\\"
         f = open(name + ".txt", "r")
         txt = f.read().split(',')[-1]
