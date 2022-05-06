66743c5ffcc8bc4f2c8abe99503279171aa46e57
lcc-666 <2502247952@qq.com>
2022-05-07 01:08:27 +0800

    查重

--- qvre.py
+++ qvre.py
@@ -19,25 +19,24 @@ for i in windows:
         dict[i] += 1
     else:
         dict[i] = 1
-print(dict)
-# clearn=[]
-# for i in dict.items():
-#     if i[-1] == 20:
-#         clearn.append(i[0])
-#     if i[-1] == 40:
-#         clearn.append(i[0])
-# for i in clearn:
-#     del dict[i]
-#
-# for a in ls:
-#     i=a
-#     i = i[:-4]
-#     while i[-1].isnumeric() is True:
-#         i = i[:-1]
-#     if i in clearn:
-#         old=r"D:\learn\picture"+"\\"+a
-#         new=r"D:\learn\picture"+r"\quan\\"+a
-#         shutil.move(old,new)
+clearn=[]
+for i in dict.items():
+    if i[-1] == 20:
+        clearn.append(i[0])
+    if i[-1] == 40:
+        clearn.append(i[0])
+for i in clearn:
+    del dict[i]
+
+for a in ls:
+    i=a
+    i = i[:-4]
+    while i[-1].isnumeric() is True:
+        i = i[:-1]
+    if i in clearn:
+        old=r"D:\learn\picture"+"\\"+a
+        new=r"D:\learn\picture"+r"\quan\\"+a
+        shutil.move(old,new)
 
 
 
