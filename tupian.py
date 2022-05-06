31559e885e7df07407499941b64a00e8329d3188
lcc-666 <2502247952@qq.com>
2022-05-06 19:08:29 +0800

    封装进入一个类

--- tupian.py
+++ tupian.py
@@ -1,58 +1,69 @@
+import base64
+import os
+import time
+
 import requests
 from lxml import etree
-import base64
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
-import time
-import os
 
-def get_list(url):
-    root_url = url
 
-    tagre_url = "{}/tupian/list-%E6%B8%85%E7%BA%AF%E5%94%AF%E7%BE%8E-1.html".format(root_url)
+class down:
+    def __init__(self, url):
+        self.bro = webdriver.Chrome()
+        self.root_url = url
+        ls = self.get_list()
+        for item in ls:
+            print(item)
+            self.detail(item)
 
-    bro = requests.session()
-    text = bro.get(tagre_url).text
-    tree = etree.HTML(text)
-    url_list = tree.xpath("//div[@id='tpl-img-content']/li/a/@href")
-    ls = []
-    for i in range(20):
-        ls.append(root_url + url_list[i])
-    return ls
+    def get_list(self):
+        root_url = self.root_url
+        tagre_url = "{}/tupian/list-%E6%B8%85%E7%BA%AF%E5%94%AF%E7%BE%8E-1.html".format(root_url)
+        bro = requests.session()
+        text = bro.get(tagre_url).text
+        tree = etree.HTML(text)
+        url_list = tree.xpath("//div[@id='tpl-img-content']/li/a/@href")
+        url_list=url_list
+        l2=list(set(url_list))
+        l2.sort(key=url_list.index)
 
-def detail(url):
-    bro =webdriver.Chrome()
-    bro.get(url)
-    WebDriverWait(bro,10,0.5).until(EC.presence_of_element_located((By.CLASS_NAME,"videopic")))
-    ls=bro.find_elements(by=By.CLASS_NAME,value="videopic")
+        ls = []
+        for i in range(20):
+            ls.append(root_url + l2[i])
 
-    for item in ls:
-        item.click()
-        time.sleep(0.5)
-        txt=item.get_property("src")
-        name=item.get_property("title")
-        file=open("{}.txt".format(name),'w')
-        file.write(txt)
+        return ls
+
+    def detail(self,url):
+        bro=self.bro
+        bro.get(url)
+        WebDriverWait(bro, 10, 0.5).until(EC.presence_of_element_located((By.CLASS_NAME, "videopic")))
+        ls = bro.find_elements(by=By.CLASS_NAME, value="videopic")
+
+        for item in ls:
+            item.click()
+            time.sleep(0.5)
+            txt = item.get_property("src")
+            name = item.get_property("title")
+            file = open("{}.txt".format(name), 'w')
+            file.write(txt)
+            file.close()
+            self.getpic(name)
+
+    def getpic(self,name):
+        path = r"D:\learn\picture\\"
+        f = open(name + ".txt", "r")
+        txt = f.read().split(',')[-1]
+        f.close()
+        imgdata = base64.b64decode(txt)
+        file = open(r"{}.jpg".format(path + name), 'wb')
+        file.write(imgdata)
         file.close()
-        getpic(name)
-
-
-def getpic(name):
-    path=r"D:\learn\picture\\"
-    f=open(name+".txt","r")
-    txt = f.read().split(',')[-1]
-    f.close()
-    imgdata = base64.b64decode(txt)
-    file = open(r"{}.jpg".format(path+name), 'wb')
-    file.write(imgdata)
-    file.close()
-    os.remove(name+".txt")
+        os.remove(name + ".txt")
 
 
 if __name__ == '__main__':
-    #ls = get_list("https://www.69fcy.com")
-    url="https://www.95531d0bd91f.com/tupian/194358.html"
-    detail(url)
+    url = "https://www.95531d0bd91f.com/"
+    down(url)
