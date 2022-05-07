8248997905014f420e3fbc3015547c4bc65cd5f4
lcc-666 <2502247952@qq.com>
2022-05-07 11:03:35 +0800

    base64转jpg

---
+++ xiourenji/base64tojpg.py
@@ -0,0 +1,20 @@
+import base64
+import os
+
+"""
+base64转jpg
+需要提供图片名称
+可选择提供路径
+"""
+
+
+def getpic(name, path=r"D:\learn\picture\\"):
+    f = open(name + ".txt", "r")
+    txt = f.read().split(',')[-1]
+    f.close()
+    imgdata = base64.b64decode(txt)
+    file = open(r"{}.jpg".format(path + name), 'wb')
+    file.write(imgdata)
+    file.close()
+    os.remove(name + ".txt")
+
