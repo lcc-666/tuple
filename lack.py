f990ddab42e5bc6973425e0c3d5e06ad5528f078
lcc-666 <2502247952@qq.com>
2022-05-12 11:49:32 +0800

    添加网址自动跟新

--- lack.py
+++ lack.py
@@ -17,11 +17,11 @@ ls = f.readlines()
 ls.reverse()
 for i in range(len(ls)):
     ls[i] = ls[i].split("/")[-1].strip()
-url = "https://www.95531d0bd91f.com/"
-root = "https://www.2ce4d130c500.com/tupian/"
+url = "https://www.95531d0bd91f.com/enter/index.html"
 ls = qvchong.deduplication(ls)
 s = dowlod(url)
-
+s.new_url()
+root=s.root_url+"/tupian/"
 for i in ls:
-    # print(root+i)
+    #print(root+i)
     s.detail(root + i)
